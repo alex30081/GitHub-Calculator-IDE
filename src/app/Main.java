c94c6f6af9c573d050ca9b22211d06d09638464e
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-05-06 09:06:08 +0100

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
-
+        System.out.println("Version 1.0");
         int a = 8;
         int b = 2;
         int c = add(a, b);
@@ -20,4 +20,5 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+
 }
